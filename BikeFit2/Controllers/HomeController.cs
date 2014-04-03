b66960f23cbaa3d4bee5271bf7f9d3bd159586df
pltaylor <pltaylor3@gmail.com>
2014-04-02 20:12:51 -0400

    Initial commit

---
+++ BikeFit2/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace BikeFit2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

d61f9dedad113268500ef805126d5aa4ab2726fe
pltaylor <pltaylor3@gmail.com>
2014-04-02 20:17:47 -0400

    add data context file

---
+++ BikeFit2/DataLayer/BikeFitContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using BikeFit2.Models;
+
+namespace BikeFit2.DataLayer
+{
+    public class BikeFitContext : DbContext 
+    {
+        public BikeFitContext() : base("AzureConnection")
+        {
+        }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<BikeModel> BikeModels { get; set; }
+
+        public DbSet<BikeSize> BikeSizes { get; set; }
+
+        public DbSet<BikeType> BikeTypes { get; set; }
+    }
+}

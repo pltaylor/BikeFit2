0dd377a0e000b26ff9d8d6a429f2e11a1aa5b070
unknown <ptaylor@ptaylor-LT.corp.rcrracing.com>
2014-08-11 22:19:16 -0400

    First go at adding drop downs

--- BikeFit2/Controllers/HomeController.cs
+++ BikeFit2/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using BikeFit2.DataLayer;
+using BikeFit2.Models.ViewModels;
 
 namespace BikeFit2.Controllers
 {
@@ -30,18 +30,9 @@ namespace BikeFit2.Controllers
 
         public ViewResult GeometryTables()
         {
-            var manufacturers = _Context.Manufacturers.Where(x=>x.IsActive).ToList();
+            var vm = new GeometryTableViewModel(_Context);
 
-            foreach (var manufacturer in manufacturers)
-            {
-                manufacturer.Models = manufacturer.Models.OrderBy(m => m.Name).ToList();
-                foreach (var model in manufacturer.Models)
-                {
-                    model.Sizes = model.Sizes.Where(m => m.Approved).OrderBy(m => m.SortOrder).ThenBy(m=>m.Size).ToList();
-                }
-            }
-            
-            return View("GeometryTables", "_SlowtwitchLayout", manufacturers);
+            return View("GeometryTables", "_SlowtwitchLayout", vm);
         }
     }
 }
---
+++ BikeFit2/Models/ViewModels/GeometryTableViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
+using BikeFit2.DataLayer;
+
+namespace BikeFit2.Models.ViewModels
+{
+    public class GeometryTableViewModel
+    {
+        public GeometryTableViewModel(BikeFitContext context)
+        {
+            ManufacturerList = new List<SelectListItem> {new SelectListItem {Text = "All", Value = "All"}};
+            Manufacturer = "All";
+
+            Manufacturers = context.Manufacturers.Where(x => x.IsActive).ToList();
+
+            foreach (var manufacturer in Manufacturers)
+            {
+                ManufacturerList.Add(new SelectListItem {Text = manufacturer.Name, Value = manufacturer.ManufacturerID.ToString()});
+
+                manufacturer.Models = manufacturer.Models.OrderBy(m => m.Name).ToList();
+                foreach (var model in manufacturer.Models)
+                {
+                    model.Sizes = model.Sizes.Where(m => m.Approved).OrderBy(m => m.SortOrder).ThenBy(m => m.Size).ToList();
+                }
+            }
+
+            BikeTypesList = new List<SelectListItem> {new SelectListItem {Text = "All", Value = "All"}};
+            BikeType = "All";
+
+            var bikeTypes = context.BikeTypes;
+            foreach (var type in bikeTypes)
+            {
+                BikeTypesList.Add(new SelectListItem { Text = type.Type, Value = type.BikeTypeId.ToString(CultureInfo.InvariantCulture) });
+            }
+        }
+
+        public List<Manufacturer> Manufacturers { get; private set; }
+
+        public string Manufacturer { get; set; }
+
+        public List<SelectListItem> ManufacturerList { get; set; }
+
+        public string BikeType { get; set; }
+
+        public List<SelectListItem> BikeTypesList { get; set; }
+    }
+}

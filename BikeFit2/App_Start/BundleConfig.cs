3310a03b6bfee95365fe78b5223740c77c472f12
pltaylor <pltaylor3@gmail.com>
2014-08-22 14:40:57 -0400

    One more to CDN

--- BikeFit2/App_Start/BundleConfig.cs
+++ BikeFit2/App_Start/BundleConfig.cs
@@ -7,16 +7,12 @@ namespace BikeFit2
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
             
             bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
-                      "~/Scripts/respond.js",
                     "~/scripts/knockout-3.2.0.debug.js",
                     "~/scripts/breeze.debug.js"));
 

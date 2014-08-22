b3aab6dda4ac4971d42c2cd64bc34372f9c9a3f3
pltaylor <pltaylor3@gmail.com>
2014-08-22 10:11:58 -0400

    Add spinner and grey to full tri bike view

--- BikeFit2/Scripts/App/fullTriBike.js
+++ BikeFit2/Scripts/App/fullTriBike.js
@@ -43,7 +43,8 @@
                 //#region Internal Methods
                 function activate() {
                     var manufacturesPromise = datacontext.lookups.manufacturers();
-                    logger.log('Frames View Activated', null, 'frames', false);
+                    logger.log('Frames View Activated', null, false);
+                    $('.preloader').remove();
 
                     return $.when(manufacturesPromise).then(function (results) {
                         manufacturers(results);

fe1370561c8a99a2395996e987d0b5d68656308d
pltaylor <pltaylor3@gmail.com>
2014-10-08 12:54:11 -0400

    change around how aerobarmodel links to other classes

--- BikeFit2/Models/Aerobar/AerobarModel.cs
+++ BikeFit2/Models/Aerobar/AerobarModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,9 +7,22 @@ namespace BikeFit2.Models.Aerobar
 {
     public class AerobarModel
     {
+        private ICollection<BaseBarWidth> _BaseBarWidths;
+        private ICollection<PadWidth> _PadWidths;
+        private ICollection<PadHeight> _PadHeights;
+        private ICollection<PadReach> _PadReaches;
+        private ICollection<AerobarHeight> _AerobarHeights;
+        private ICollection<Stem> _Stems;
+
         public AerobarModel()
         {
             AerobarID = Guid.NewGuid();
+            _BaseBarWidths = new List<BaseBarWidth>();
+            _PadWidths = new List<PadWidth>();
+            _PadHeights = new List<PadHeight>();
+            _PadReaches = new List<PadReach>();
+            _AerobarHeights = new List<AerobarHeight>();
+            _Stems = new List<Stem>();
         }
 
         [Key]
@@ -19,36 +33,78 @@ namespace BikeFit2.Models.Aerobar
 
         public virtual AerobarManufacturer AerobarManufacturer { get; set; }
 
-        [ForeignKey("BaseBarWidth")]
-        public Guid BaseBarWidthID { get; set; }
-
-        public virtual BaseBarWidth BaseBarWidth { get; set; }
+        public virtual ICollection<BaseBarWidth> BaseBarWidths
+        {
+            get
+            {
+                return _BaseBarWidths;
+            }
+            set
+            {
+                _BaseBarWidths = value;
+            }
+        }
 
         public AeroBarType AeroBarType { get; set; }
 
-        [ForeignKey("PadWidth")]
-        public Guid PadWidthID { get; set; }
+        public virtual ICollection<PadWidth> PadWidths
+        {
+            get
+            {
+                return _PadWidths;
+            }
+            set
+            {
+                _PadWidths = value;
+            }
+        }
 
-        public virtual PadWidth PadWidth { get; set; }
+        public virtual ICollection<PadHeight> PadHeights
+        {
+            get
+            {
+                return _PadHeights;
+            }
+            set
+            {
+                _PadHeights = value;
+            }
+        }
 
-        [ForeignKey("PadHeight")]
-        public Guid PadHeightID { get; set; }
+        public virtual ICollection<PadReach> PadReaches
+        {
+            get
+            {
+                return _PadReaches;
+            }
+            set
+            {
+                _PadReaches = value;
+            }
+        }
 
-        public virtual PadHeight PadHeight { get; set; }
+        public virtual ICollection<AerobarHeight> AerobarHeights
+        {
+            get
+            {
+                return _AerobarHeights;
+            }
+            set
+            {
+                _AerobarHeights = value;
+            }
+        }
 
-        [ForeignKey("PadReach")]
-        public Guid PadReachID { get; set; }
-
-        public virtual PadReach PadReach { get; set; }
-
-        [ForeignKey("AerobarHeight")]
-        public Guid AerobarHeightID { get; set; }
-
-        public virtual AerobarHeight AerobarHeight { get; set; }
-
-        [ForeignKey("Stem")]
-        public Guid StemID { get; set; }
-
-        public virtual Stem Stem { get; set; }
+        public virtual ICollection<Stem> Stems
+        {
+            get
+            {
+                return _Stems;
+            }
+            set
+            {
+                _Stems = value;
+            }
+        }
     }
 }

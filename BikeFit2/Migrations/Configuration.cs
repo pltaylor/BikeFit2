84ffedf52f6d986ee91775ae97d8237797529871
pltaylor <pltaylor3@gmail.com>
2014-10-11 18:30:53 -0400

    Add db seed for aerobar manufacturers

--- BikeFit2/Migrations/Configuration.cs
+++ BikeFit2/Migrations/Configuration.cs
@@ -21,6 +21,13 @@ namespace BikeFit2.Migrations
               new AeroBarType { AeroBarTypeId = 4, Type = "Aerobar & Basebar & Stem" }
             );
 
+            context.AeroBarManufacturers.AddOrUpdate(
+                m => m.Name,
+                new AerobarManufacturer { Name = "Tririg" },
+                new AerobarManufacturer { Name = "Zipp" },
+                new AerobarManufacturer { Name = "3T" },
+                new AerobarManufacturer { Name = "Profile Design" },
+                new AerobarManufacturer { Name = "Vision" });
         }
     }
 }

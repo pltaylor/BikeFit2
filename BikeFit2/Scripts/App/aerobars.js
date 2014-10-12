8dc808a37572729b97e0896369fa22fef635e6a1
pltaylor <pltaylor3@gmail.com>
2014-10-12 14:05:42 -0400

    first 2 drop downs seem to work

--- BikeFit2/Scripts/App/aerobars.js
+++ BikeFit2/Scripts/App/aerobars.js
@@ -56,47 +56,27 @@
             }
 
             function createAerobar(name, canvasName, color) {
-                var bikeType = ko.observable();
-
-                bikeType.subscribe(function () {
-                    if (models().length != 0) {
-                        models.removeAll();
-                    }
-                    if (sizes().length != 0) {
-                        sizes.removeAll();
-                    }
-                    manufacturer(manufacturers()[0]);
-                });
-
+                
                 var manufacturer = ko.observable();
                 manufacturer.subscribe(function (newValue) {
-                    datacontext.getBikeModels(models, newValue.manufacturerID(), bikeType());
+                    datacontext.getAerobarModels(models, newValue.manufacturerID());
+                    model(models()[0]);
                 });
 
                 var models = ko.observableArray();
                 var model = ko.observable();
                 model.subscribe(function (newValue) {
                     if (newValue != null) {
-                        datacontext.getUniqueBikeSizes(sizes, newValue.bikeModelID());
+                        drawFrame(newValue);
                     }
-                    size(sizes()[0]);
-                });
-
-                var sizes = ko.observableArray();
-                var size = ko.observable();
-                size.subscribe(function (newValue) {
-                    drawFrame(newValue);
                 });
 
                 var output = {
                     color: color,
-                    bikeType: bikeType,
                     manufacturer: manufacturer,
                     models: models,
                     model: model,
                     name: name,
-                    sizes: sizes,
-                    size: size
                 };
                 return output;
 
@@ -106,64 +86,64 @@
                     if (drawingCanvas.getContext) {
                         var ctx = drawingCanvas.getContext('2d');
                         // clear
-                        ctx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
-                        if (localSize == null) {
-                            return;
-                        }
-                        // Create the rear wheel
-                        ctx.beginPath();
-                        ctx.arc(localSize.rearWheelXloc(), localSize.rearWheelYloc(), localSize.wheelRadius(), 0, Math.PI * 2, true);
-                        ctx.closePath();
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create front wheel
-                        ctx.beginPath();
-                        ctx.arc(localSize.frontWheelXloc(), localSize.frontWheelYloc(), localSize.wheelRadius(), 0, Math.PI * 2, true);
-                        ctx.closePath();
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create rear chainstay
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.rearWheelXloc(), localSize.rearWheelYloc());
-                        ctx.lineTo(localSize.bbXloc(), localSize.bbYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create seat tube
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.seatTubeXloc(), localSize.seatTubeYloc());
-                        ctx.lineTo(localSize.bbXloc(), localSize.bbYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create seat stay
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.seatTubeXloc(), localSize.seatTubeYloc());
-                        ctx.lineTo(localSize.rearWheelXloc(), localSize.rearWheelYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create downtube
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.bbXloc(), localSize.bbYloc());
-                        ctx.lineTo(localSize.headTubeBottomXloc(), localSize.headTubeBottomYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create toptube
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.seatTubeXloc(), localSize.seatTubeYloc());
-                        ctx.lineTo(localSize.headTubeTopXloc(), localSize.headTubeTopYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create headtube
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.headTubeTopXloc(), localSize.headTubeTopYloc());
-                        ctx.lineTo(localSize.headTubeBottomXloc(), localSize.headTubeBottomYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
-                        // create fork
-                        ctx.beginPath();
-                        ctx.moveTo(localSize.frontWheelXloc(), localSize.frontWheelYloc());
-                        ctx.lineTo(localSize.headTubeBottomXloc(), localSize.headTubeBottomYloc());
-                        ctx.strokeStyle = color;
-                        ctx.stroke();
+                        //ctx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+                        //if (localSize == null) {
+                        //    return;
+                        //}
+                        //// Create the rear wheel
+                        //ctx.beginPath();
+                        //ctx.arc(localSize.rearWheelXloc(), localSize.rearWheelYloc(), localSize.wheelRadius(), 0, Math.PI * 2, true);
+                        //ctx.closePath();
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create front wheel
+                        //ctx.beginPath();
+                        //ctx.arc(localSize.frontWheelXloc(), localSize.frontWheelYloc(), localSize.wheelRadius(), 0, Math.PI * 2, true);
+                        //ctx.closePath();
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create rear chainstay
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.rearWheelXloc(), localSize.rearWheelYloc());
+                        //ctx.lineTo(localSize.bbXloc(), localSize.bbYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create seat tube
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.seatTubeXloc(), localSize.seatTubeYloc());
+                        //ctx.lineTo(localSize.bbXloc(), localSize.bbYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create seat stay
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.seatTubeXloc(), localSize.seatTubeYloc());
+                        //ctx.lineTo(localSize.rearWheelXloc(), localSize.rearWheelYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create downtube
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.bbXloc(), localSize.bbYloc());
+                        //ctx.lineTo(localSize.headTubeBottomXloc(), localSize.headTubeBottomYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create toptube
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.seatTubeXloc(), localSize.seatTubeYloc());
+                        //ctx.lineTo(localSize.headTubeTopXloc(), localSize.headTubeTopYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create headtube
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.headTubeTopXloc(), localSize.headTubeTopYloc());
+                        //ctx.lineTo(localSize.headTubeBottomXloc(), localSize.headTubeBottomYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
+                        //// create fork
+                        //ctx.beginPath();
+                        //ctx.moveTo(localSize.frontWheelXloc(), localSize.frontWheelYloc());
+                        //ctx.lineTo(localSize.headTubeBottomXloc(), localSize.headTubeBottomYloc());
+                        //ctx.strokeStyle = color;
+                        //ctx.stroke();
                     }
                 }
             }
